e26fff67b25213645e6250d0ce960764c0b06f7a
Nour Malaeb <nourmalaeb@gmail.com>
2025-04-28 00:56:43 -0400

    Better progress bar using Radix

--- components/audio-progress-bar.js
+++ components/audio-progress-bar.js
@@ -1,21 +1,41 @@
 import './audio-progress-bar.css';
+import { Slider } from 'radix-ui';
 
 export default function AudioProgressBar(props) {
   const { duration, progress, name, onSeek, isPlaying } = props;
 
+  // return (
+  //   <input
+  //     type="range"
+  //     name={name}
+  //     // style={progressStyles}
+  //     onChange={e => {
+  //       onSeek(e.target.value);
+  //     }}
+  //     defaultValue={progress}
+  //     min={0}
+  //     max={duration}
+  //     step={0.01}
+  //     className={`audio-progress-bar${isPlaying ? ' is-playing' : ''}`}
+  //   />
+
+  // );
   return (
-    <input
-      type="range"
-      name={name}
-      // style={progressStyles}
-      onChange={e => {
-        onSeek(e.target.value);
-      }}
-      defaultValue={progress}
-      min={0}
+    <Slider.Root
+      className={`audio-progress-bar${isPlaying ? ' is-playing' : ''}`}
+      defaultValue={0}
+      value={[progress]}
       max={duration}
       step={0.01}
-      className={`audio-progress-bar${isPlaying ? ' is-playing' : ''}`}
-    />
+      onValueChange={values => onSeek(values[0])}
+    >
+      <Slider.Track className="audio-progress-track">
+        <Slider.Range className="audio-progress-range" />
+      </Slider.Track>
+      <Slider.Thumb
+        className="audio-progress-seeker"
+        aria-label="Track progress"
+      />
+    </Slider.Root>
   );
 }
