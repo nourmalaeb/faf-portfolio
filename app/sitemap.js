fce04f1fa8cf169b771bd1f9c81bd1666b3601da
Nour Malaeb <nourmalaeb@gmail.com>
2025-05-26 10:41:56 +0200

    Add sitemap.js

---
+++ app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://www.firasfiras.com',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
+  ];
+}
