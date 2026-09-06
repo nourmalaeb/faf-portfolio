a3dd85362d9570814b0173a6f0478fee86bdb6db
Nour Malaeb <nourmalaeb@gmail.com>
2026-09-06 14:40:21 -0400

    cleanup

--- next.config.js
+++ next.config.js
@@ -2,16 +2,10 @@ module.exports = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'v5.airtableusercontent.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'cdn.sanity.io',
-        port: '',
-        pathname: '/images/bgkim0us/production/**',
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        port: "",
+        pathname: "/images/bgkim0us/production/**",
       },
     ],
   },
